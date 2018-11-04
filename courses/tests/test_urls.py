a4b0ba3f840714648ce5ed021b2b2749a973e00f
nadyrbek97 <nadyrbek97@gmail.com>
2018-11-04 20:24:50 +0600

    added requirements.txt

--- courses/tests/test_urls.py
+++ courses/tests/test_urls.py
@@ -16,8 +16,20 @@ class TestCourse(TestCase):
         course.branches.create(address='Bishek', latitude='23.23.23', longitude='343.232')
         self.assertTrue(isinstance(course, Course))
 
+    def test_create_branch(self):
+        branch = Branch.objects.create()
+        branch.address = 'Osh'
+        branch.latitude = '12122.3232'
+        branch.longitude = '2332.2323'
+        self.assertTrue(isinstance(branch, Branch))
+
+    def test_create_contact(self):
+        contact = Contact.objects.create()
+        contact.type = 'PHONE'
+        contact.value = '0555025045'
+        self.assertTrue(isinstance(contact, Contact))
+
 
 class TestRestFramework(APITestCase):
-
     pass
 
